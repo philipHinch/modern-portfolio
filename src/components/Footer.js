efd036a9464c15fc34eefb890ae23ea91999d3e3
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-04-28 21:15:07 +0300

    added go to top link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
+//icons
+import { Icon } from '@iconify/react';
+
 const Footer = () => {
     return (
         <footer>
+            <a href="#" className="goToTop" title='Go to top'><Icon className='upArrowIcon' icon="bi:arrow-up-square-fill" /></a>
             <small>Copyright &copy; 2022 - <a href="https://github.com/philipHinch" className="githubLink" target="_blank">https://github.com/philipHinch</a></small>
         </footer>
     );
